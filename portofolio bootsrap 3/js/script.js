0ab57e7e327c8df817f996208b3aa8a24acb2244
firdi <audifirdi@gmail.com>
2022-08-22 23:56:20 +0700

    menambahkan file

---
+++ portofolio bootsrap 3/js/script.js
@@ -0,0 +1,17 @@
+// Event pada saat link
+$('.page-scroll').on('click', function (e) {
+
+// ambil isi href
+var tujuan = $(this).attr('href');
+// Tangkap Elemen yang bersangkutan
+var elemenTujuan = $(tujuan);
+
+
+// Pindahkan Scroll
+$('body').animate({
+    scrollTop: elemenTujuan.offset().top
+});
+
+e.preventDefault();
+
+});
